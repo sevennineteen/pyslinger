17b0c6ac4b2b11d6d33531a75d83ccb77e7eaaa2
Patric M DelCioppo <pdelcioppo@gmail.com>
2011-06-26 08:52:17 -0400

    Added support for loading whole pages in single request

--- pyj-slinger.py
+++ pyj-slinger.py
@@ -1,11 +1,20 @@
 import sys
 import codecs
+import base64
 import simplejson as json
 from helpers import *
+from odict import odict
 
-#------ CUSTOM PARAMS // pass in via command line---- -------------------------------------
-assert len(sys.argv) == 5, "Supply cq_server, username, password, payload_path when calling this script."
-cq_server, username, password, payloads_path = sys.argv[1:]
+#------ CUSTOM PARAMS // pass in via command line -----------------------------------------
+assert len(sys.argv) >= 5, "Supply cq_server, username, password, payload_path, [mode] when calling this script."
+cq_server, username, password, payloads_path = sys.argv[1:5]
+
+try:
+    mode = sys.argv[5]
+except:
+    mode = 'itemwise'
+assert mode in ['itemwise', 'nodewise'], "Supported modes are 'itemwise' and 'nodewise'."
+print '> Loading %s...' % mode
 
 headers = cq_auth_header(username, password) # username, password for CQ user
 json_paths = get_file_list(payloads_path, '.*\.json') # directory containing JSON payloads
@@ -34,19 +43,76 @@ def populate_node(path, properties, **kwargs):
     
     post_multipart(path, fields, files, headers, **kwargs)
 
-def main():
-    "Iterate through all JSON payloads, passing each node's path and properties."
-    for jp in json_paths:
-        payload = json.loads(read_file(jp))
+def slingify(nodes):
+    "Returns the node contents as Sling-ready JSON (and any binary file nodes)."
     
+    file_nodes = []
+    
+    def transcribe_node(node):
+        "Transcribes nodes into a nested data structure."
+        
+        # add the node's own properties
+        node_name = node['path'].split('/')[-1]
+        node_data = odict((p['name'], p['value']) for p in node['properties'] if not p.get('type', None) == 'nt:file')
+        
+        # add any binary nodes to file list
+        if any(p.get('type', None) == 'nt:file' for p in node['properties']):
+            file_nodes.append(node)
+        
+        # add its child nodes
+        for c in get_children(node):
+            node_data.update(transcribe_node(c))
+        
+        return {node_name: node_data}
+    
+    def get_children(parent):
+        "Returns children of the supplied node."
+        return [    n for n in nodes 
+                    if parent['path'] + '/' in n['path'] 
+                    and '/' not in n['path'][len(parent['path']) + 1:]    ]
+    
+    slingified_json = json.dumps(transcribe_node(sorted(nodes, key=lambda n:n['path'])[0]))
+    return slingified_json, file_nodes
+
+def main(mode):
+    "Iterates through all JSON payloads, dispatching load according to supplied mode."
+        
+    def itemwise():
+        "Loads content item and nodes in a single request, except for binary assets."
+        
+        # assemble content substructure and add to payload properties
+        slingified_json, file_nodes = slingify(payload['nodes'])
+        
+        new_props = (   (':operation', 'import'),
+                        (':contentType', 'json'),
+                        (':content', slingified_json)
+                        )
+        map(lambda p: payload['properties'].append({'name': p[0], 'value': p[1]}), new_props)
+
         # populate the page
-        base_path = cq_server + payload['path']
         populate_node(base_path, payload['properties'], label='  Content item')
+        
+        # populate binaries
+        for node in file_nodes:
+            node_path = '/'.join([base_path, node['path']])
+            populate_node(node_path, node['properties'], label='    Binary')
     
+    def nodewise():
+        "Loads content item and each node's content as a separate request."
+        
+        # populate the page
+        populate_node(base_path, payload['properties'], label='  Content item')
+        
         # populate the nodes
         for node in payload['nodes']:
             node_path = '/'.join([base_path, node['path']])
             populate_node(node_path, node['properties'], label='    Node')
+    
+    for jp in json_paths:
+        payload = json.loads(read_file(jp))
+        base_path = cq_server + payload['path']
+        
+        locals()[mode]() # call function corresponding to mode
 
 if __name__ == "__main__":
-    main()
+    main(mode)
--- run_demo.sh
+++ run_demo.sh
@@ -1,3 +1,4 @@
 # Usage: 
-# python pyj-slinger.py [cq_server] [username] [password] [payloads_path]
-python pyj-slinger.py http://localhost:4502 admin admin ./payloads
+# python pyj-slinger.py cq_server username password payloads_path [mode]
+python pyj-slinger.py http://localhost:4502 admin admin ./payloads itemwise
+python pyj-slinger.py http://localhost:4502 admin admin ./payloads nodewise
