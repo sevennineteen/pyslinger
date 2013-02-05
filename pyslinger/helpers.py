a668b79aaedaa13150c2aa9b6d0a9a164005be05
Patric M DelCioppo <pdelcioppo@gmail.com>
2013-02-04 23:09:50 -0500

    return response message as string

--- pyslinger/helpers.py
+++ pyslinger/helpers.py
@@ -28,7 +28,7 @@ def post_commentator(func):
         node = args[0]
 
         status = int(response[0]['status'])
-        message = soup.title.text,
+        message = soup.title.text[0] if soup.title.text else None,
         error = None if status in [200, 201] else soup.find('div', id='Message').text
 
         return DotDict({
